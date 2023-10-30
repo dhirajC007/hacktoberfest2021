a0da354efc7d615070c9266e1fcbaa6bb4f22ae0
dhirajC007 <142651203+dhirajC007@users.noreply.github.com>
2023-10-30 05:41:14 -0700

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,33 @@
+public class BinarySearch {
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid; // Element found, return its index
+            } else if (array[mid] < target) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // Target is in the left half
+            }
+        }
+
+        return -1; // Element not found
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
